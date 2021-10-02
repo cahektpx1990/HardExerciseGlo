b8e3ce943a1b6cd0a1dd032b00cb693ca7412bbc
Alex Losev <cahektpx1990@gmail.com>
2021-10-02 13:43:00 +0300

    hardlesson02 done

---
+++ script.js
@@ -0,0 +1,21 @@
+let num = 266219;
+
+const numMas = Array.from(String(num), Number);
+console.log(numMas);
+
+let mult = 1;
+
+for (let i = 0; i < numMas.length; ++i) {
+
+  mult = mult * numMas[i];
+}
+
+console.log(mult);
+
+mult **= 2;
+
+console.log(mult);
+
+mult = String(mult);
+
+console.log(mult.substring(0, 2));
