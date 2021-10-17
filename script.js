b42ce281c2023ffac3cb29ff6a6a33472c47eca5
Alex Losev <cahektpx1990@gmail.com>
2021-10-17 17:35:40 +0300

    lesson07 done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+const day = new Date();
+let dayWeek = day.getDay();
+
+if (dayWeek === 0) {
+  dayWeek = 6;
+} else { dayWeek--; }
+
+week.forEach((day, i) => {
+  let str = day;
+  if (i === dayWeek) {
+    str = `<b>${day}</b>`;
+  } else {
+    str = `${day}`;
+  }
+  if (i === 5 || i === 6) { str = `<i>${str}</i>`; }
+  document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
+});
