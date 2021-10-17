93271b78dc51517e3193445be390c521cd0b6d00
Alex Losev <cahektpx1990@gmail.com>
2021-10-17 16:15:51 +0300

    hardlesson04 done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const getString = function(varia) {
+  let string = 'Строка нужна';
+
+  if (typeof varia === 'string') {    
+    const str = varia.trim();
+    if(str.length > 30) {
+      string = str.substring(0, 30) + '....';
+    } else {
+      string = str;
+    }
+  }
+  return string;
+};
+
+console.log(getString('qwer qwer qwer qer'));
+console.log(getString(0));
+console.log(getString('qwer qwer qwer qerw qwe qwe qwer qwer qewr qwer qewr qewr'));
